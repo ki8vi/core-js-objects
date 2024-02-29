b125c91bc7c65ad0a003e547f411716b0fedd951
ki8vi <oneofbln@gmail.com>
2024-02-29 18:55:29 +0600

    feat: add solution for removeProperties task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -64,8 +64,14 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const copy = obj;
+  for (let i = 0; i < keys.length; i += 1) {
+    if (keys[i] in obj) {
+      delete copy[keys[i]];
+    }
+  }
+  return copy;
 }
 
 /**
