8355007dd3291fa48c20463ce59658c9b09267d8
ki8vi <oneofbln@gmail.com>
2024-02-29 22:46:04 +0600

    feat: add solution for fromJSON task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -224,8 +224,10 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const parsed = JSON.parse(json);
+  const outputObj = Object.setPrototypeOf(parsed, proto);
+  return outputObj;
 }
 
 /**
