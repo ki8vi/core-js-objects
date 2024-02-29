7ce5843a5d5da8b859d14bff614a70a269db2786
ki8vi <oneofbln@gmail.com>
2024-02-29 18:59:30 +0600

    feat: add solution for compareObjects task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -86,8 +86,10 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const stringifyed1 = JSON.stringify(obj1);
+  const stringifyed2 = JSON.stringify(obj2);
+  return stringifyed1 === stringifyed2;
 }
 
 /**
